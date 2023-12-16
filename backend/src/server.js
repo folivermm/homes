41a996e160d6941d0402515266c95286586b4b5e
folivermm <foliverm@gmail.com>
2023-12-16 00:27:12 -0800

    server code update

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,34 @@
+const { PORT = 5001 } = process.env;
+
 const app = require("./app");
 const knex = require("./db/connection");
 
-const PORT = process.env.PORT || 5001;
+const listener = () => console.log(`Listening on Port ${PORT}!`);
+
 knex.migrate
     .latest()
     .then((migrations) => {
-        console.log("Migrations:", migrations);
-        const server = app.listen(PORT, () => {
-            console.log(`Server is running on Port ${PORT}`);
-        });
+        console.log("migrations", migrations);
+        app.listen(PORT, listener);
     })
-    .catch((error) => {
-        console.error("Error running migrations:", error);
-        process.exit(1);
-    });
+    .catch(console.error);
+
+
+
+
+// const app = require("./app");
+// const knex = require("./db/connection");
+
+// const PORT = process.env.PORT || 5001;
+// knex.migrate
+//     .latest()
+//     .then((migrations) => {
+//         console.log("Migrations:", migrations);
+//         const server = app.listen(PORT, () => {
+//             console.log(`Server is running on Port ${PORT}`);
+//         });
+//     })
+//     .catch((error) => {
+//         console.error("Error running migrations:", error);
+//         process.exit(1);
+//     });
