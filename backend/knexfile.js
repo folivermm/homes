e86df388d4f109479b6910684868ed12e6382497
folivermm <foliverm@gmail.com>
2023-12-16 00:19:40 -0800

    knexfile comment out production

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -20,16 +20,16 @@ module.exports = {
             directory: path.join(__dirname, "src", "db", "seeds"),
         },
     },
-    production: {
-        client: "postgresql",
-        connection: DATABASE_URL,
-        pool: { min: 0, max: 5 },
-        migrations: {
-            directory: path.join(__dirname, "src", "db", "migrations"),
-        },
-        seeds: {
-            directory: path.join(__dirname, "src", "db", "seeds"),
-        },
-    },
+    // production: {
+    //     client: "postgresql",
+    //     connection: DATABASE_URL,
+    //     pool: { min: 0, max: 5 },
+    //     migrations: {
+    //         directory: path.join(__dirname, "src", "db", "migrations"),
+    //     },
+    //     seeds: {
+    //         directory: path.join(__dirname, "src", "db", "seeds"),
+    //     },
+    // },
 }
 
